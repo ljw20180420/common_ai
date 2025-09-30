6b6865675a0c339492998d4c80029639ecc6d360
ljw <ljw2017@sjtu.edu.cn>
2025-09-30 10:41:06 +0800

    Add dataset abstract and component configs

---
+++ common_ai/dataset.py
@@ -0,0 +1,27 @@
+from abc import ABC, abstractmethod
+import os
+
+
+class MyDatasetAbstract:
+    def __init__(
+        self,
+        data_file: os.PathLike,
+        name: str,
+        test_ratio: float,
+        validation_ratio: float,
+        seed: int,
+    ):
+        self.data_file = os.fspath(data_file)
+        self.name = name
+        self.test_ratio = test_ratio
+        self.validation_ratio = validation_ratio
+        self.seed = seed
+
+    @abstractmethod
+    def __call__(self):
+        pass
+
+    @classmethod
+    @abstractmethod
+    def hpo(self):
+        pass
