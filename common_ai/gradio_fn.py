5fe5ad0f354ed981453e90acd040af90f1f0187c
ljw <ljw2017@sjtu.edu.cn>
2025-11-20 16:48:09 +0800

    MyGradioFnAbstract support huggingface repo_id as checkpoints_path and logs_path.

--- common_ai/gradio_fn.py
+++ common_ai/gradio_fn.py
@@ -1,7 +1,10 @@
+import os
 import importlib
 import jsonargparse
 import pathlib
 from abc import ABC, abstractmethod
+from huggingface_hub import HfFileSystem, hf_hub_download
+from tbparse import SummaryReader
 
 
 class MyGradioFnAbstract(ABC):
@@ -14,9 +17,48 @@ class MyGradioFnAbstract(ABC):
         self.inference_dict = {}
         for test_cfg, inference_cfg in zip(app_cfg.test, app_cfg.inference):
             test_cfg = test_cfg.init_args
-            checkpoints_path = pathlib.Path(test_cfg.checkpoints_path)
+
+            if not os.path.exists(test_cfg.logs_path):
+                # no local logs, assume logs_path as repo_id
+                repo_id = test_cfg.logs_path
+                hfs = HfFileSystem()
+                train_logs = hfs.glob(f"{repo_id}/logs/train/*")
+                assert len(train_logs) == 1, "train log not found or not unique"
+                test_cfg.logs_path = pathlib.Path(
+                    hf_hub_download(
+                        repo_id=repo_id,
+                        filename=f"logs/train/{train_logs[0].rsplit('/', 1)[-1]}",
+                        repo_type="model",
+                    )
+                ).parent.parent.as_posix()
+
+            df_train = SummaryReader(f"{test_cfg.logs_path}/train", pivot=True).scalars
+            best_epoch = (
+                df_train["step"]
+                .iloc[df_train[f"eval/{test_cfg.target}"].argmin()]
+                .item()
+            )
+
+            if not os.path.exists(test_cfg.checkpoints_path):
+                # no local checkpoints, assume checkpoints_path as repo_id
+                repo_id = test_cfg.checkpoints_path
+                hfs = HfFileSystem()
+                checkpoint_files = hfs.glob(
+                    f"{repo_id}/checkpoints/checkpoint-{best_epoch}/*"
+                )
+                for checkpoint_file in checkpoint_files:
+                    test_cfg.checkpoints_path = pathlib.Path(
+                        hf_hub_download(
+                            repo_id=repo_id,
+                            filename=f"checkpoints/checkpoint-{best_epoch}/{checkpoint_file.rsplit('/', 1)[-1]}",
+                            repo_type="model",
+                        )
+                    ).parent.parent.as_posix()
+
             train_cfg = train_parser.parse_path(
-                checkpoints_path / "checkpoint-0" / "train.yaml"
+                pathlib.Path(test_cfg.checkpoints_path)
+                / f"checkpoint-{best_epoch}"
+                / "train.yaml"
             )
             _, preprocess, _, model_cls = train_cfg.model.class_path.rsplit(".", 3)
             data_name = train_cfg.dataset.init_args.name
