7adb397a3ccac0d39397ed1b69b4fad57c89394c
ljw <ljw2017@sjtu.edu.cn>
2026-03-03 12:00:20 +0800

    Add ElasticNet as a regularizor to utils.py

--- common_ai/utils.py
+++ common_ai/utils.py
@@ -1,10 +1,13 @@
-from torch import nn
+import importlib
 import os
 import pathlib
-import numpy as np
+
 import datasets
-import importlib
 import jsonargparse
+import numpy as np
+from einops.layers.torch import EinMix
+from torch import nn
+
 from .model import MyModelAbstract
 
 
@@ -59,6 +62,32 @@ class Residual(nn.Module):
 
     def forward(self, x):
         return self.module(x) + x
+
+
+class ElasticNet(nn.Module):
+    def __init__(self, reg_l1: float, reg_l2: float) -> None:
+        super().__init__()
+        self.reg_l1 = reg_l1
+        self.reg_l2 = reg_l2
+
+    def forward(self, model: nn.Module):
+        elastic_net_loss = 0.0
+        for module in model.modules():
+            if (
+                isinstance(module, nn.Linear)
+                or isinstance(module, EinMix)
+                or isinstance(module, nn.Conv1d)
+                or isinstance(module, nn.Conv2d)
+                or isinstance(module, nn.Conv3d)
+            ):
+                if not module.weight.requires_grad:
+                    continue
+                elastic_net_loss += (
+                    self.reg_l1 * module.weight.flatten().abs().sum()
+                    + self.reg_l2 * module.weight.flatten().pow(2).sum()
+                )
+
+        return elastic_net_loss
 
 
 class SeqTokenizer:
