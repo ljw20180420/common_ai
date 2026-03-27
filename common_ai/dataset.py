efea08f89a0c64438566f0bf3ae6fe1aef3e7dab
ljw <ljw2017@sjtu.edu.cn>
2026-03-27 13:14:22 +0800

    Make MyDatasetAbstract more abstract.

--- common_ai/dataset.py
+++ common_ai/dataset.py
@@ -1,21 +1,12 @@
 from abc import ABC, abstractmethod
-import os
 
 
 class MyDatasetAbstract(ABC):
     def __init__(
         self,
-        data_file: os.PathLike,
         name: str,
-        test_ratio: float,
-        validation_ratio: float,
-        seed: int,
     ):
-        self.data_file = os.fspath(data_file)
         self.name = name
-        self.test_ratio = test_ratio
-        self.validation_ratio = validation_ratio
-        self.seed = seed
 
     @abstractmethod
     def __call__(self):
