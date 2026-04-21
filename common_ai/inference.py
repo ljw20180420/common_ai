c15dab0fe3602ec6956a75319947d8cf65fe029a
ljw <ljw2017@sjtu.edu.cn>
2026-04-21 10:03:13 +0800

    Add load_model method for MyInferenceAbstract

--- common_ai/inference.py
+++ common_ai/inference.py
@@ -1,5 +1,10 @@
 from abc import ABC, abstractmethod
-
+import jsonargparse
+from .test import MyTest
 
 class MyInferenceAbstract(ABC):
-    pass
+    def load_model(self, test_cfg: jsonargparse.Namespace, train_parser: jsonargparse.ArgumentParser) -> None:
+        _, train_cfg, self.logger, self.model, self.my_generator = MyTest(
+            **test_cfg.as_dict()
+        ).load_model(train_parser)
+        self.batch_size = train_cfg.train.batch_size
