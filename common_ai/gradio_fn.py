00f6be94ac7692422084590f7d033294fed8da7a
ljw <ljw2017@sjtu.edu.cn>
2026-04-21 10:13:07 +0800

    Define load_inference for MyGradioFnAbstract

--- common_ai/gradio_fn.py
+++ common_ai/gradio_fn.py
@@ -1,13 +1,12 @@
 import importlib
-import os
 import pathlib
 import tempfile
 from abc import ABC, abstractmethod
 
 import jsonargparse
-from huggingface_hub import HfFileSystem, hf_hub_download
-from tbparse import SummaryReader
 
+from .test import MyTest
+from .inference import MyInferenceAbstract
 
 class MyGradioFnAbstract(ABC):
     def __init__(
@@ -20,72 +19,23 @@ class MyGradioFnAbstract(ABC):
         self.inference_dict = {}
         for test_cfg, inference_cfg in zip(app_cfg.test, app_cfg.inference):
             test_cfg = test_cfg.init_args
-
-            if not os.path.exists(test_cfg.logs_path):
-                # no local logs, assume logs_path as repo_id
-                repo_id = test_cfg.logs_path
-                hfs = HfFileSystem()
-                train_logs = hfs.glob(f"{repo_id}/logs/train/*")
-                assert len(train_logs) == 1, "train log not found or not unique"
-                test_cfg.logs_path = pathlib.Path(
-                    hf_hub_download(
-                        repo_id=repo_id,
-                        filename=f"logs/train/{train_logs[0].rsplit('/', 1)[-1]}",
-                        repo_type="model",
-                    )
-                ).parent.parent.as_posix()
-
-            df_train = SummaryReader(f"{test_cfg.logs_path}/train", pivot=True).scalars
-            if test_cfg.maximize_target:
-                best_epoch = (
-                    df_train["step"]
-                    .iloc[df_train[f"eval/{test_cfg.target}"].argmax()]
-                    .item()
-                )
-            else:
-                best_epoch = (
-                    df_train["step"]
-                    .iloc[df_train[f"eval/{test_cfg.target}"].argmin()]
-                    .item()
-                )
-
-            if not os.path.exists(test_cfg.checkpoints_path):
-                # no local checkpoints, assume checkpoints_path as repo_id
-                repo_id = test_cfg.checkpoints_path
-                hfs = HfFileSystem()
-                checkpoint_files = hfs.glob(
-                    f"{repo_id}/checkpoints/checkpoint-{best_epoch}/*"
-                )
-                for checkpoint_file in checkpoint_files:
-                    test_cfg.checkpoints_path = pathlib.Path(
-                        hf_hub_download(
-                            repo_id=repo_id,
-                            filename=f"checkpoints/checkpoint-{best_epoch}/{checkpoint_file.rsplit('/', 1)[-1]}",
-                            repo_type="model",
-                        )
-                    ).parent.parent.as_posix()
-
-            train_cfg = train_parser.parse_path(
-                pathlib.Path(test_cfg.checkpoints_path)
-                / f"checkpoint-{best_epoch}"
-                / "train.yaml"
-            )
+            my_test = MyTest(**test_cfg.as_dict())
+            _, train_cfg = my_test.load_train_cfg(train_parser)
             _, preprocess, _, model_cls = train_cfg.model.class_path.rsplit(".", 3)
             data_name = train_cfg.dataset.init_args.name
             repo_id = f"{preprocess}_{model_cls}_{data_name}"
             self.inference_dict[repo_id] = (inference_cfg, test_cfg)
 
-        self.repo_id = None
+    def load_inference(self, repo_id: str) -> MyInferenceAbstract:
+        assert repo_id in self.inference_dict, f"repo id {repo_id} is not found"
+        inference_cfg, test_cfg = self.inference_dict[repo_id]
+        inference_module, inference_cls = inference_cfg.class_path.rsplit(".", 1)
+        my_inference = getattr(
+            importlib.import_module(inference_module), inference_cls
+        )(**inference_cfg.init_args.as_dict())
+        my_inference.load_model(test_cfg, self.train_parser)
 
-    def reload_inference(self, repo_id: str):
-        if repo_id != self.repo_id:
-            assert repo_id in self.inference_dict, f"repo id {repo_id} is not found"
-            self.repo_id = repo_id
-            inference_cfg, self.test_cfg = self.inference_dict[repo_id]
-            inference_module, inference_cls = inference_cfg.class_path.rsplit(".", 1)
-            self.my_inference = getattr(
-                importlib.import_module(inference_module), inference_cls
-            )(**inference_cfg.init_args.as_dict())
+        return my_inference
 
     @abstractmethod
     def launch(self):
